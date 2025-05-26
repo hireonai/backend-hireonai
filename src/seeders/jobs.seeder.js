b71570b10dc7a48c2f6fea294f4de154aedcd472
lotusCupcake <fikriansari.mfa@gmail.com>
2025-05-26 19:13:26 +0700

    fix: dataset and seeder

--- src/seeders/jobs.seeder.js
+++ src/seeders/jobs.seeder.js
@@ -8,32 +8,33 @@ const parseNumber = (value) => {
   return parseInt(String(value).replace(/[^\d]/g, ""), 10);
 };
 
-const parseArray = (value) => {
-  const matches = value.match(/\[(.*)\]/);
-  if (!matches) return [];
-  return matches[1].split(",").map((v) => v.trim().replace(/'/g, ""));
-};
-
 async function seedJobs({ categoryMap, experienceMap, companyMap }) {
   try {
     await mongoose.connect(mongodbUri);
     await Job.deleteMany();
     console.log("Jobs collection cleared!");
 
-    const mappedJobs = data.map((job) => ({
-      categories: parseArray(job.kategori).map((k) => categoryMap[k]),
-      url: job.url,
-      jobPosition: job.job_position,
-      employmentType: job.employment_type,
-      workingLocationType: job.working_location_type,
-      workingLocation: job.working_location,
-      minExperienceId: experienceMap[job.min_experience.trim()],
-      minSalary: parseNumber(job.min_salary.trim()),
-      maxSalary: parseNumber(job.max_salary.trim()),
-      jobDescList: parseArray(job.job_desc_list),
-      jobQualificationsList: parseArray(job.job_qualification_list),
-      companyId: companyMap[job.company_name.trim()],
-    }));
+    const mappedJobs = data
+      .map((job) => {
+        if (job.salary !== "Negotiable") {
+          return {
+            categories: job.kategori.map((k) => categoryMap[k]),
+            url: job.url,
+            jobPosition: job.job_position,
+            employmentType: job.employment_type,
+            workingLocationType: job.working_location_type,
+            workingLocation: job.working_location,
+            minExperienceId: experienceMap[job.min_experience.trim()],
+            minSalary: parseNumber(job.min_salary.trim()),
+            maxSalary: parseNumber(job.max_salary.trim()),
+            jobDescList: job.job_desc_list,
+            jobQualificationsList: job.job_qualification_list,
+            companyId: companyMap[job.company_name.trim()],
+          };
+        }
+        return null;
+      })
+      .filter((job) => job !== null);
 
     await Job.insertMany(mappedJobs);
     console.log("Jobs seeded!");
